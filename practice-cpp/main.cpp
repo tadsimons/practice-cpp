bd15b569c0fa617cf87296ed8c4673e7c58eed4f
张新朋 <zhangxinpeng@moqipobing.com>
2020-03-11 16:48:21 +0800

    Initial Commit

---
+++ practice-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice-cpp
+//
+//  Created by wanba on 2020/3/11.
+//  Copyright © 2020 yangda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
